7012fdd5cd4af606625f55e46699abf76d2cc1c9
René Rössler <rene@freshx.de>
2013-08-01 01:43:37 +0200

    oops, forgot an ifdef

--- fuse4js.cc
+++ fuse4js.cc
@@ -624,7 +624,9 @@ static void DispatchOp(uv_async_t* handle, int status)
     argv[argc++] = String::New(f4js_cmd.u.setxattr.value);
     argv[argc++] = Number::New((double)f4js_cmd.u.setxattr.size);
     argv[argc++] = Number::New((double)f4js_cmd.u.setxattr.position);
+#ifdef __APPLE__
     argv[argc++] = Number::New((double)f4js_cmd.u.setxattr.options);
+#endif
     break;
   
   case OP_RENAME:
